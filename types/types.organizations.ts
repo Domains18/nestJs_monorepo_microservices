6c42f18352923868dc42894ce6a1c54b33e3d1c2
Domains18 <gibsonsgibson88@gmail.com>
2023-12-22 16:32:13 +0300

    interfaces and types feat: prisma updates

--- types/types.organizations.ts
+++ types/types.organizations.ts
@@ -1,8 +1,41 @@
-
-
-type organization =[] | {
+type organization = [] | {
     id: number,
     name: string,
     description: string,
     address: string,
+    phone: string,
+    users?: [],
+    organization_id?: number,
+    transactions?: [],
+    created_at?: string,
+    updated_at?: string | null
+}
+
+type user = [] | {
+    firstName: string,
+    lastName: string,
+    email: string,
+    id: number,
+    password: string,
+    organizationName: string,
+    role: string,
+    organization_id: number,
+}
+
+type transaction = [] | {
+    id: number,
+    amount: number,
+    description: string,
+    organization_id: number,
+    created_at: string,
+    updated_at: string | null
+}
+
+
+type role = [] | {
+    id: number,
+    name: string,
+    description: string,
+    created_at: string,
+    updated_at: string | null
 }
