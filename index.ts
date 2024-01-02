26b0a5d084bb762376c428c39a2530c7125d8c34
Domains18 <gibsonsgibson88@gmail.com>
2024-01-02 15:00:21 +0300

    removed tsconfig strict property and admin create update functionality

--- index.ts
+++ index.ts
@@ -1,17 +1,38 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import http from 'http';
-import {errorHandler} from "./middlewares/errorHandler";
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+dotenv.config();
 const app = express();
+const prisma = new PrismaClient();
+app.use(cookieParser());
 
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
+app.use(compression());
 app.use(express.json());
-app.use(cors({ credentials: true, origin: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-
 
 const server = http.createServer(app);
+
+Promise.resolve()
+  .then(async () => {
+    await prisma.$connect();
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  })
+  .finally(async () => {
+    server.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  });
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,20 +1,22 @@
 export interface Organization {
-    organizationName: string;
-    description: string;
-    website: string;
-    email: string;
-    phone: String;
-    logo: string;
-    users?: [];
-    vendors?: [];
+  organizationName: string;
+  description: string;
+  website: string;
+  email: string;
+  phone: String;
+  logo: string;
+  users?: [];
+  vendors?: [];
 }
 
-export interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    organization: string;
-    vendor: string;
-
+export interface Admin {
+  id?: number;
+  organizationName: string;
+  vendorName: string;
+  email: string;
+  phone: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  username: string;
 }
